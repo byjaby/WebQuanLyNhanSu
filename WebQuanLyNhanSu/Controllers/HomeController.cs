19f1771381940b4a2b608adcac63df71191c4e19
phamm <phamm@DESKTOP-UF5EHCP>
2024-03-02 10:22:34 +0700

    Add project files.

---
+++ WebQuanLyNhanSu/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebQuanLyNhanSu.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult QLCheDo()
+        {
+            return View();
+        }
+        public ActionResult QLNghiViec()
+        {
+            return View();
+        }
+        public ActionResult GuiMail()
+        {
+            return View();
+        }
+        public ActionResult PhongBan()
+        {
+            return View();
+        }
+        public ActionResult BoPhan()
+        {
+            return View();
+        }
+        public ActionResult BangCong()
+        {
+            return View();
+        }
+        public ActionResult KTKL()
+        {
+            return View();
+        }
+        public ActionResult BangLuong()
+        {
+            return View();
+        }
+        public ActionResult TDBangLuong()
+        {
+            return View();
+        }
+        public ActionResult TinhLuong()
+        {
+            return View();
+        }
+        public ActionResult DuAn()
+        {
+            return View();
+        }
+        public ActionResult PhanCongDuAn()
+        {
+            return View();
+        }
+        public ActionResult NghiPhep()
+        {
+            return View();
+        }
+        public ActionResult BaoCaoNhanSu()
+        {
+            return View();
+        }
+        public ActionResult BaoCaoLuong()
+        {
+            return View();
+        }
+        public ActionResult TraCuu()
+        {
+            return View();
+        }
+        public ActionResult HeThong()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        public ActionResult ThemTK()
+        {
+            return View();
+        }
+        public ActionResult SuaTK()
+        {
+            return View();
+        }
+        public ActionResult SuaPB()
+        {
+            return View();
+        }
+        public ActionResult ThemPB()
+        {
+            return View();
+        }
+    }
+}
